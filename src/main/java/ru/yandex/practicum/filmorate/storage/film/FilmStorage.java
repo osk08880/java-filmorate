65b13007ab8644667da983dbede21d1c58e12eec
Aleh Kavalkou <osk08880@yandex.ru>
2025-09-14 23:25:35 +0300

    .

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film update(Film film);
+
     Collection<Film> findAll();
+
     Film findById(Long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,7 +6,10 @@ import java.util.Collection;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User user);
+
     Collection<User> findAll();
+
     User findById(Long id);
 }
